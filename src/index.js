df8fa46e53ad9e5dce5caefdbba83f775e8da011
shelser <shelserg@gmail.com>
2025-11-27 18:46:17 +0300

    added new files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './index.html';
 
-import './index.scss'
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './index.scss';
+
+const videoBG = document.querySelector('.video-bg');
+
+videoBG.innerHTML = `
+    <source src="video/video.webm" type="video/webm">
+    <source src="video/video.mp4" type="video/mp4">
+`;
