d59ff5c50055a725671339ea0e469c25f4d45fad
shelser <shelserg@gmail.com>
2025-12-01 10:49:12 +0300

    added new files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,45 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 import './index.scss';
+import { slidersInit } from './modules/slider';
+
+slidersInit('.about__slider', {
+    pagination: {
+        el: '.about__slider-pagination',
+    }
+});
+
+slidersInit('.career__slider', {
+    breakpoints: {
+    // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            pagination: {
+                enabled: true  // включено на мобильных
+            }
+        },
+    // when window width is >= 480px
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+            pagination: {
+                enabled: false  // включено на мобильных
+            }
+        },
+    // when window width is >= 640px
+        1240: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+            pagination: {
+                enabled: false  // включено на мобильных
+            }
+        },
+    },   
+    pagination: {
+        el: '.career__slider-pagination',
+    }
+})
 
 const videoBG = document.querySelector('.video-bg');
 
